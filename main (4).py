5b869f0c8ec8f4fdf00c4119193a34a000fc8e91
Ajith Kumar <114465935+akofficial211@users.noreply.github.com>
2023-09-20 12:28:13 +0530

    Add files via upload

--- main (4).py
+++ main (4).py
@@ -1,5 +1,5 @@
 class BankAccount:
-    def __init__(self, account_number, account_holder_name, initial_balance):
+    def _init_(self, account_number, account_holder_name, initial_balance):
         self.__account_number = account_number
         self.__account_holder_name = account_holder_name
         self.__account_balance = initial_balance
@@ -20,6 +20,3 @@ class BankAccount:
 
     def display_balance(self):
         return f"Account balance for {self.__account_holder_name}: ${self.__account_balance}"
-
-# Creating an instance of the BankAccount class
-my_account = BankAccount("12345", "John Doe", 1000)
